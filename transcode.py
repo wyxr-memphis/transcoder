54c833a0ae55b3e452903865386b87663f8d82f2
Robby Grant <fourgrant@gmail.com>
2023-05-06 11:33:15 -0500

    added genre and also choice to make before the import

--- transcode.py
+++ transcode.py
@@ -21,36 +21,50 @@ metadata_file_path = config['metadata_file_path']
 with open(metadata_file_path, 'r') as f:
     lines = f.readlines()
 
+# Get the user's choice for the genre
+print("Please choose a genre for this import batch:")
+print("1. Rock")
+print("2. Funk and Soul")
+print("3. Pop")
+print("4. Hip-hop")
+print("5. Electronic")
+
+choice = int(input("Enter the number corresponding to your choice: "))
+genres = ["Rock", "Funk and Soul", "Pop", "Hip-hop", "Electronic"]
+selected_genre = genres[choice - 1]
+
 # Iterate through each line in the metadata file
 for line in lines:
     # Split the line into parts
     parts = line.strip().split(',')
 
-    # Check if the line has a year
+    # Check if the line has a date
     if len(parts) == 5:
-        wav_file_name, artist, album, file_name, year = parts
+        wav_file_name, artist, album, file_name, date = parts
     else:
         wav_file_name, artist, album, file_name = parts
-        year = ""
+        date = ""
 
     # Replace 'DA' with 'FUS' in the wav_file_name
     input_wav_file_name = wav_file_name.replace('DA', 'FUS')
 
     # Define input and output file paths
     input_file = os.path.join(input_directory, input_wav_file_name + '.wav')
+
+    # Construct the output file name with artist, album, and desired output file name
     output_file_name = f"{artist} - {album} - {file_name}"
     output_file = os.path.join(output_directory, output_file_name + '.m4a')
 
     # Make sure the output directory exists
     os.makedirs(output_directory, exist_ok=True)
 
-    # Set the title metadata to just the song name with the wav file name (FUS prefix)
-    title = os.path.splitext(file_name)[0]
+    # Set the title metadata to include artist, album, and song title
+    title = f"{artist} - {album} - {file_name}"
 
     # Transcode the WAV file to AAC using FFmpeg with a reduced bitrate and add metadata
     ffmpeg_command = [
         'ffmpeg', '-i', input_file, '-vn', '-c:a', 'aac', '-b:a', '96k',
-        '-metadata', f'artist={artist}', '-metadata', f'album={album}', '-metadata', f'title={title}', '-metadata', f'date={year}', output_file
+        '-metadata', f'artist={artist}', '-metadata', f'album={album}', '-metadata', f'title={title}', '-metadata', f'year={date}', '-metadata', f'genre={selected_genre}', output_file
     ]
 
     subprocess.run(ffmpeg_command)
